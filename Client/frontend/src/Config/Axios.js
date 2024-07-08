e64ff59ed07612b0ada87c90384c20cf1463402c
Mohammed Shan <150375093+Shhaan@users.noreply.github.com>
2024-07-08 17:59:42 +0530

    Delete Client/frontend directory

--- Client/frontend/src/Config/Axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios" 
-import { Api_base } from "./Constants"
-
-const instance = axios.create({
-    baseURL:Api_base
-})
-export default instance
-
- 
--- Client/frontend/src/Config/Constants.js
+++
@@ -1,6 +0,0 @@
-export const  Api_base  = "http://127.0.0.1:8000/"
-
-
-export  const BASE_APP_URL = "http://localhost:3000" 
-export const GOOGLE_OAUTH2_CLIENT_ID = '64469589733-m3hqdmjm2rf9ucihqqpgbhjjn86t0k8e.apps.googleusercontent.com'
-export const GOOGLE_OAUTH2_CLIENT_SECRET = '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'
